7108e71b10e994e8546e514f05b0274f49846935
Jesse Lin <jesse.lin67@gmail.com>
2020-05-05 01:42:21 -0400

    minor naming changes

---
+++ commands/basic_functions.py
@@ -0,0 +1,21 @@
+import discord
+import os
+import sys
+from discord.ext import commands
+
+sys.path.append('/home/jesse/projects/rembot')
+
+class Basic(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+    
+    @commands.command()
+    async def hello(self, ctx, *, member: discord.Member = None):
+        member = member or ctx.author
+        if self._last_member is None or self._last_member.id != member.id:
+            await ctx.send(f'Hello {member.mention}~')
+        else:
+            await ctx.send(f'Hello {member.mention}... this feels familiar...')
+        self._last_member = member
+    
