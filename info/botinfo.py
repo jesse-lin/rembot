53cbd7cfa314eb427324d1ed83af25a5d6846087
Jesse Lin <jesse.lin67@gmail.com>
2020-05-07 04:30:12 -0400

    jsonify bot info

--- info/botinfo.py
+++ info/botinfo.py
@@ -1,14 +1,38 @@
 import discord
 from discord.ext import commands
 import json
+import os
+import datetime
 
-async def about(bot, message):
-    embed = discord.Embed(title="RemBot", description=bot.description)
-    embed.set_thumbnail(url="https://i.imgur.com/oNUY7dx.jpg")
-    embed.add_field(name="Author", value='<@260913181734469655>')
-    embed.add_field(name="Server count", value=f'{len(bot.guilds)}')
-    embed.add_field(name="Date created", value='2020/04/30')
-    # embed.set_image(url="https://i.imgur.com/oNUY7dx.jpg")
+def check_json(bot, file):
+    try:
+        data = json.load(file)
+    except ValueError:
+        data = dict()
+        info = dict()
+        data['Title'] = 'RemBot'
+        data['Description'] = bot.description
+        info['Author'] = '<@260913181734469655>'
+        info['Server count'] = len(bot.guilds)
+        info['Date created'] = datetime.datetime(2020, 4, 30, 17, 55, 000000).isoformat()
+        info['Last accessed'] = datetime.datetime.now().isoformat()
+        data['info'] = info
+        data['thumbnail'] = "https://i.imgur.com/oNUY7dx.jpg"
+        data['commands'] = dict()
+        json.dump(data, file, indent=4)
+    info['Server count'] = len(bot.guilds)
+    info['Last accessed'] = datetime.datetime.now().isoformat()
+    output = json.dumps(data)
+    return output
+
+
+async def about(str1: str, message):
+    data = json.loads(str1)
+    embed = discord.Embed(title=data['Title'], description=data['Description'])
+    embed.set_thumbnail(url=data['thumbnail'])
+    for key in data['info']:
+        embed.add_field(name=f'{key}', value=data['info'][f'{key}'], inline=False)
+        # embed.set_image(url="https://i.imgur.com/oNUY7dx.jpg")
 
     await message.channel.send(content="About me:", embed=embed)
 
--- main.py
+++ main.py
@@ -15,6 +15,8 @@ bot = commands.Bot(command_prefix=BOT_PREFIX, description='A discord bot designe
 bot.add_cog(default.Default(bot))
 bot.add_cog(botinfo.BotInfo(bot))
 
+
+
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}{bot.user.id}------')
@@ -26,7 +28,10 @@ async def on_message(message):
         return
 
     if message.content == f'<@!{bot.user.id}>':
-        await botinfo.about(bot, message)
+        f = open('data/bot.json', "w+")
+        output = botinfo.check_json(bot, f)
+        f.close()
+        await botinfo.about(output, message)
 
     await bot.process_commands(message)
 
